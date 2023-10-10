4745aa947ca04c3f1e1475f99d0a49e090048575
Dell-User <Dell-User@DESKTOP-GAPN8T9>
2023-10-10 10:15:53 +0200

    Add project files.

---
+++ 19.Constructors/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.InteropServices;
+
+namespace _19.Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Book book1 = new Book("Harry Potter","JK Rowling",400) ;
+            Console.WriteLine(book1.title);
+            Console.ReadLine();
+        }
+    }
+}
+
+    class Book
+{
+    public string title;
+    public string author;
+    public int pages;
+    public Book(string aTitle, string aAuthor, int aPages)
+    {
+        title = aTitle;
+        author = aAuthor;
+        pages = aPages;
+    }
+
+}
+
